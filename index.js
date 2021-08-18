1a94d87463315ee4ca5d99275079c24c1a847615
Francesco Marciani <marciani3@gmail.com>
2021-08-18 12:14:11 +0200

    first

---
+++ index.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+
+const store = {};
+
+const addGlobalStateEntry = (key) => {
+  store[key] = {
+    setState: (val) => setGlobalState(key, val),
+    listeners: new Set(),
+  };
+};
+
+export const setGlobalState = (key, val, dontEmit = false) => {
+  if (key in store) {
+    store[key].state = val;
+    if (!dontEmit) {
+      store[key].listeners.forEach((fn) => fn(val));
+    }
+  } else {
+    addGlobalStateEntry(key);
+  }
+};
+
+export const getGlobalState = (key) => {
+  return (key in store && store[key].state) || undefined;
+};
+
+const useGlobalState = (key) => {
+  if (!(key in store)) {
+    addGlobalStateEntry(key);
+  }
+
+  useEffect(() => {
+    return () => {
+      store[key].listeners.delete(setState);
+    };
+  }, [key]);
+
+  const [state, setState] = useState(store[key].state);
+  store[key].listeners.add(setState);
+
+  return [state, store[key].setState];
+};
+
+export default useGlobalState;
